e82ef35387e1e428d8848d3d40451118da121607
lucas <lucas_developer@163.com>
2022-06-02 14:22:21 +0800

    init commit

---
+++ app/src/main/java/com/lucas/fanexclusive/App.kt
@@ -0,0 +1,15 @@
+package com.lucas.fanexclusive
+
+import android.app.Application
+
+/**
+ * @author lucas
+ * @since 2022/6/2.
+ * @des
+ */
+class App: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/com/lucas/fanexclusive/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.lucas.fanexclusive
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+/**
+ * @date 2022/6/2
+ * @author lucas
+ * @des
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
